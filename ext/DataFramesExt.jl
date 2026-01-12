ed61fba4bed031a91d45caec10669677abc009dd
singularitti <singularitti@outlook.com>
2026-01-12 04:05:39 -0600

    Fix a bug in `compute_flops`

--- ext/DataFramesExt.jl
+++ ext/DataFramesExt.jl
@@ -1,9 +1,10 @@
 module DataFramesExt
 
 using DataFrames: DataFrame
+using Tables: rows
 
 import NsightCompute: compute_flops
 
-compute_flops(data::DataFrame) = DataFrame(compute_flops(data))
+compute_flops(data::DataFrame) = DataFrame(compute_flops(rows(data)))
 
 end
