e80ac266ee4c8ad9ce8b3763c9d9df9956c7cad0
singularitti <singularitti@outlook.com>
2026-01-14 20:36:30 -0600

    Extend `compute_ai` in ext/DataFramesExt.jl

--- ext/DataFramesExt.jl
+++ ext/DataFramesExt.jl
@@ -3,8 +3,10 @@ module DataFramesExt
 using DataFrames: DataFrame
 using Tables: rows
 
-import NsightCompute: compute_flops
+import NsightCompute: compute_flops, compute_ai
 
 compute_flops(data::DataFrame) = DataFrame(compute_flops(rows(data)))
+
+compute_ai(data::DataFrame) = DataFrame(compute_ai(rows(data)))
 
 end
